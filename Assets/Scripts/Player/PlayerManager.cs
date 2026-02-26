082f057768678aa4a7508963f38ea386a79d427f
ACM-Sh4dow <km3391@my.acm.ac.uk>
2026-02-26 12:51:23 +0000

    FEATURE: Camera Switching

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +8,7 @@ public class PlayerManager : MonoBehaviour
 {
     public Vector3[] spawnPositions;
     public GameObject[] playerPrefabs;
+    public static List<ProtagonistController> players = new List<ProtagonistController>();
 
     public static ProtagonistController player;
 
@@ -13,22 +17,36 @@ public class PlayerManager : MonoBehaviour
         for (int i = 0; i < 4; i++)
         {
             var spawned = Instantiate(playerPrefabs[i], spawnPositions[i], Quaternion.identity, this.transform);
-
-            if (i == 0)
-            {
-                player = spawned.GetComponent<ProtagonistController>();
-            }
+            spawned.name = spawned.name.Substring(0, 8);
+            players.Add(spawned.GetComponent<ProtagonistController>());
         }
+        player = players[0];
     }
 
     public static void SyncCameraMove(Vector2 input)
     {
-        Debug.Log("Called");
-        Debug.Log($"camera move input is {input.x}, {input.y}");
-
         switch (player.gameObject.name)
         {
+            case "Player 1":
+                if (input == new Vector2(1,0)) { player = players[1]; }
+                else if (input == new Vector2(0,-1)) { player = players[2]; }
 
+                break;
+            case "Player 2":
+                if (input == new Vector2(-1, 0)) { player = players[0]; }
+                else if (input == new Vector2(0, -1)) { player = players[3]; }
+
+                break;
+            case "Player 3":
+                if (input == new Vector2(1, 0)) { player = players[3]; }
+                else if (input == new Vector2(0, 1)) { player = players[0]; }
+
+                break;
+            case "Player 4":
+                if (input == new Vector2(-1, 0)) { player = players[2]; }
+                else if (input == new Vector2(0, 1)) { player = players[1]; }
+
+                break;
         }
     }
 
